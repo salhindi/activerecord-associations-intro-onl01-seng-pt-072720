a05410821db94dd30949dbd4f070a652d1fac7e4
Sara A <calisaraalhindi@gmail.com>
2020-10-14 21:28:03 +0000

    Automatically backed up by Learn

--- db/migrate/02_create_genres.rb
+++ db/migrate/02_create_genres.rb
@@ -1,6 +1,6 @@
-class CreateArtists < ActiveRecord::Migration[4.2]
+class CreateGenres < ActiveRecord::Migration[4.2]
   def change
-    create_table :artists do |t|
+    create_table :genres do |t|
       t.string :name
     end
   end
